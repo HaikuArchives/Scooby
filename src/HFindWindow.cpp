6c7f777392b0bd3ca49e0e8957d4af1ff20c7ce7
atakamat <atakamat>
2001-03-12 02:19:04 +0000

    *** empty log message ***

--- src/HFindWindow.cpp
+++ src/HFindWindow.cpp
@@ -15,7 +15,7 @@ enum{
  * Constructor
  ***********************************************************/
 HFindWindow::HFindWindow(BRect rect,const char* name)
-		:BWindow(rect,name,B_FLOATING_WINDOW,0)
+		:BWindow(rect,name,B_FLOATING_WINDOW,B_ASYNCHRONOUS_CONTROLS)
 		,fTarget(NULL)
 		,fCanQuit(false)
 {
