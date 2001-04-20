e6607260181d539a105b68eeddc8a0f4884877a3
atakamat <atakamat>
2001-04-20 21:09:56 +0000

    *** empty log message ***

--- src/HGeneralSettingView.h
+++ src/HGeneralSettingView.h
@@ -3,6 +3,10 @@
 
 #include <View.h>
 
+enum{
+	M_FONT_CHANGED = 'MFNC'
+};
+
 class HGeneralSettingView :public BView {
 public:
 					HGeneralSettingView(BRect rect);
@@ -13,5 +17,6 @@ protected:
 	virtual void	MessageReceived(BMessage *message);
 	virtual	void	AttachedToWindow();
 private:
+	
 };
 #endif
