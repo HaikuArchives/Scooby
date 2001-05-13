42a6d674261d0682843659deeb2348f2338cac3e
atakamat <atakamat>
2001-05-13 10:24:45 +0000

    Initial checkin

---
+++ src/utf7.h
@@ -0,0 +1,16 @@
+#ifndef __UTF7_H__
+#define __UTF7_H__
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+//! Convert IMAP4 modified UTF7 to UTF8
+void IMAP4UTF72UTF8(char *dst, char *src);
+//! Convert UTF8 to IMAP4 modified UTF7
+void UTF8IMAP4UTF7(char *dst, char *src);
+
+#ifdef __cplusplus
+};
+#endif
+#endif /* __BASE64_H__ */
