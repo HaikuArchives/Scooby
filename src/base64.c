931ebaba1c2c1f3b209ae6ecd7d1f5bff02d763f
atakamat <atakamat>
2002-02-13 16:02:25 +0000

    *** empty log message ***

--- src/base64.c
+++ src/base64.c
@@ -1,117 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <Debug.h>
 
 static char index_64[128] = {
-	-1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
-	-1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
-	-1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,62, -1,-1,-1,63,
-	52,53,54,55, 56,57,58,59, 60,61,-1,-1, -1, 0,-1,-1,
-	-1, 0, 1, 2, 3, 4, 5, 6,  7, 8, 9,10,  11,12,13,14,
-	15,16,17,18, 19,20,21,22, 23,24,25,-1, -1,-1,-1,-1,
-	-1,26,27,28, 29,30,31,32, 33,34,35,36, 37,38,39,40,
-	41,42,43,44, 45,46,47,48, 49,50,51,-1, -1,-1,-1,-1
+    -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,62, -1,-1,-1,63,
+    52,53,54,55, 56,57,58,59, 60,61,-1,-1, -1,-1,-1,-1,
+    -1, 0, 1, 2,  3, 4, 5, 6,  7, 8, 9,10, 11,12,13,14,
+    15,16,17,18, 19,20,21,22, 23,24,25,-1, -1,-1,-1,-1,
+    -1,26,27,28, 29,30,31,32, 33,34,35,36, 37,38,39,40,
+    41,42,43,44, 45,46,47,48, 49,50,51,-1, -1,-1,-1,-1
 };
 
-#define CHAR64(c)  (((c) < 0 || (c) > 127) ? -1 : index_64[(c)])
-
-ssize_t encode64(char *out,char *_in, unsigned inlen)
+ssize_t encode64(char *out,char *_in, unsigned len)
 {
-	unsigned char oval;
-	int len;
-	unsigned char *in;
-	char *buf;
-	static char basis64[] =	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/???????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????";
- 	
- 	buf = malloc(inlen+1);
- 	in = (unsigned char*)buf;
-	
- 	memcpy(buf,_in,inlen);
- 	buf[inlen] = '\0';
- 	
- 	len = 0;
- 	
- 	for (; inlen >= 3; inlen -= 3){
-        *out++ = basis64[in[0] >> 2];
-        *out++ = basis64[((in[0] << 4) & 0x30) | (in[1] >> 4)];
-        *out++ = basis64[((in[1] << 2) & 0x3c) | (in[2] >> 6)];
-        *out++ = basis64[in[2] & 0x3f];
-     	len+=4;
-        in += 3;
+ 	static char base[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+ 	int i,r,tmp;
+	unsigned char* p;
+   
+	if( len == 0 ){
+        out[0] = '\0';
+        return 0;
     }
-    if (inlen > 0) {
-        *out++ = basis64[in[0] >> 2];
-        oval = (in[0] << 4) & 0x30;
-        if (inlen > 1) oval |= in[1] >> 4;
-        *out++ = basis64[oval];
-        *out++ = (inlen < 2) ? '=' : basis64[(in[1] << 2) & 0x3c];
-        *out++ = '=';
-        len += 4;
+
+    p = (unsigned char*) _in;
+    for( r = i = 0; i < len; ){
+        tmp = p[i++];
+        tmp <<= 8;
+        if( i < len ) tmp += p[i];
+        tmp <<= 8;
+        i++;
+        if( i < len ) tmp += p[i];
+        i++;
+
+        out[r++] = base[(tmp & 0x00FC0000) >> 18];
+        out[r++] = base[(tmp & 0x0003F000) >> 12];
+        out[r++] = base[(tmp & 0x00000FC0) >> 6 ];
+        out[r++] = base[(tmp & 0x0000003F) >> 0 ];
+        
+        if( i > len ) out[r-1] = '=';
+        if( i > len+1 ) out[r-2] = '=';
     }
-    *out = '\0';
-    free(buf);
-    return len;
+
+    out[r] = '\0';
+	return r;
 }
 
 
-ssize_t decode64(char *out,const char *_in, unsigned inlen)
+ssize_t decode64(char *out,const char *in, unsigned len)
 {
-	unsigned lup;
-	ssize_t len;
-	int c1, c2, c3, c4;
-	char *buf;
-	unsigned char *in;
- 	unsigned i,k;
- 	
- 	buf = malloc(inlen+1);
- 	in = (unsigned char*)buf;
-	
- 	k=0;
- 	// Eliminate invalid charactors.
-	for(i = 0;i < inlen;i++)
- 	{
- 		c1 = _in[i];
- 		if(CHAR64(c1) == -1)
- 			continue;
- 		buf[k++] = c1;
- 	}
- 	buf[k] = '\0';
- 	len = 0;
- 	inlen = k;
-    
-    if (in[0] == '+' && in[1] == ' ') in+=2;
-	
-	for (lup=0;lup<inlen/4;lup++)
-    {
-    	if(*in == '\r'|| *in == '\n') 
-        { 
-          	in++;
-          	continue; 
-        }
-     
-        c1 = *in++;
-        if(CHAR64(c1) == -1 ) {PRINT(("Invalid1:%d\n",c1));}
-        c2 = *in++;
-        if(CHAR64(c2) == -1 ) {PRINT(("Invalid2:%d\n",c2));}
-        c3 = *in++;
-        if(CHAR64(c3) == -1 ) {PRINT(("Invalid3:%d\n",c3));}
-        c4 = *in++;
-        if(CHAR64(c4) == -1 ) {PRINT(("Invalid4:%d\n",c4));}
-        *out++ = (CHAR64(c1) << 2) | (CHAR64(c2) >> 4);
-        len++;
-        if (c3 != '=')
-        {
-            *out++ = ((CHAR64(c2) << 4) & 0xf0) | (CHAR64(c3) >> 2);
-            len++;
-            if (c4 != '=')
-            {
-                *out++ = ((CHAR64(c3) << 6) & 0xc0) | CHAR64(c4);
-                len++;
-            }
-        }
+    int i, n;
+    char *p;
+    int tmp;
+
+    if( in == NULL || len < 0 ){
+        return -1;
     }
-    *out='\0'; /* terminate string */
- 	free(buf);
- 	return len;
+
+ 	if( len == 0 ){
+        out[0] = '\0';
+        return 0;
+    }
+
+    p = (char *)in;
+    for( n = i = 0; i < len; i += 4){
+        tmp = index_64[ (unsigned int)p[i] ];
+        tmp *= 64;
+        tmp += index_64[ (unsigned int)p[i+1] ];
+        tmp *= 64;
+        if( p[i+2] != '=' ) tmp += index_64[ (unsigned int)p[i+2] ];
+        tmp *= 64;
+        if( p[i+3] != '=' ) tmp += index_64[ (unsigned int)p[i+3] ];
+
+        out[n++] = ( tmp & 0x00FF0000 ) >> 16;
+        out[n++] = ( tmp & 0x0000FF00 ) >> 8;
+        out[n++] = ( tmp & 0x000000FF ) >> 0;
+    }
+
+    return n-1;
 }
